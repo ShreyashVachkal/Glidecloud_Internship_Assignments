86f22e56ffbcfda643619ba97488de8feea30e9a
ShreyashVachkal <vachkal1415@gmail.com>
2026-01-07 15:25:02 +0530

    Refactor app.py for improved readability

--- 07-01-2026/vector-db-fastapi/app.py
+++ 07-01-2026/vector-db-fastapi/app.py
@@ -3,24 +3,21 @@ from pydantic import BaseModel
 import chromadb
 import ollama
 
-# Create FastAPI app
+
 app = FastAPI()
 
-# Create ChromaDB client (local)
 chroma_client = chromadb.Client()
 
-# Create or get a collection
 collection = chroma_client.get_or_create_collection(
     name="documents"
 )
 
-# Request body structure
 class DocumentRequest(BaseModel):
     text: str
 
 @app.post("/add-document")
 def add_document(doc: DocumentRequest):
-    # Generate embedding using Ollama
+    
     response = ollama.embeddings(
         model="nomic-embed-text",
         prompt=doc.text
@@ -28,7 +25,7 @@ def add_document(doc: DocumentRequest):
 
     embedding = response["embedding"]
 
-    # Store in ChromaDB
+    
     collection.add(
         documents=[doc.text],
         embeddings=[embedding],
@@ -36,3 +33,4 @@ def add_document(doc: DocumentRequest):
     )
 
     return {"message": "Document stored successfully","embedding":embedding}
+
