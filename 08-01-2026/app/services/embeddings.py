a149ef623567d098ed12eaefa03fb62c8026d709
ShreyashVachkal <vachkal1415@gmail.com>
2026-01-08 16:21:46 +0530

    Add files via upload

---
+++ 08-01-2026/app/services/embeddings.py
@@ -0,0 +1,8 @@
+import ollama
+
+def generate_embedding(text: str):
+    response = ollama.embeddings(
+        model="nomic-embed-text",
+        prompt=text
+    )
+    return response["embedding"]
